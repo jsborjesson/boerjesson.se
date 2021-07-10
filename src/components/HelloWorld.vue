fb78ad8fb80644d78de944e464f06564b0f05847
Jimmy Börjesson <jimmy@boerjesson.se>
2021-07-10 16:44:01 +0200

    Add CV link buttons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,6 +17,29 @@
     <p style="text-indent: 2.5rem;">
       Jag är ansluten till Kvadrat, Sveriges lyckligaste konsulter. Vi erbjuder våra kunder en unik kombination av konsultens expertis och egenföretagarens engagemang, attityd och motivation att alltid lyckas. Kvadrat bildades 1988 och idag är vi ca 500 Kvadratare med kontor i Stockholm, Göteborg, Malmö, Linköping, Norrköping, Jönköping, Växjö, Örebro, Borlänge och Sundsvall.
     </p>
+
+    <div class="flex flex-col justify-end space-y-4 sm:space-y-0 sm:flex-row sm:space-x-4 mt-6">
+      <a
+        href="https://www.kvadrat.se/anlita-kvadrat/hitta-konsult/jimmy-borjesson/" alt="CV Jimmy Börjesson"
+        class="flex py-3 px-6 bg-white text-black font-bold rounded"
+      >
+        <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+          <path d="M0 0h24v24H0V0z" fill="none"/>
+          <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"/>
+        </svg>
+        Läs mitt CV
+      </a>
+      <a
+        href="https://www.kvadrat.se/anlita-kvadrat/hitta-konsult/jimmy-borjesson/" alt="CV Jimmy Börjesson"
+        class="flex py-3 px-6 bg-white text-black font-bold rounded"
+      >
+        <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+          <path fill="none" d="M0 0h24v24H0z"/>
+          <path d="M18 15v3H6v-3H4v3c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-3h-2zm-1-4l-1.41-1.41L13 12.17V4h-2v8.17L8.41 9.59 7 11l5 5 5-5z"/>
+        </svg>
+        CV som PDF
+      </a>
+    </div>
   </section>
 </template>
 
