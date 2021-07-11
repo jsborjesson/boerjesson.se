691003dab5b9bc536c24ce1cdcff9704d7a52ebf
Jimmy Börjesson <jimmy@boerjesson.se>
2021-07-11 10:46:53 +0200

    Add DOCX CV download, deemphasize the button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,23 +25,23 @@
     <div class="flex flex-col justify-end space-y-4 sm:space-y-0 sm:flex-row sm:space-x-4 mt-6">
       <a
         href="https://www.kvadrat.se/anlita-kvadrat/hitta-konsult/jimmy-borjesson/" alt="CV Jimmy Börjesson"
-        class="flex py-3 px-6 bg-white text-black font-bold rounded"
+        class="flex py-3 px-6 bg-white text-black font-bold rounded shadow"
       >
-        <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+        <svg class="fill-current mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
           <path d="M0 0h24v24H0V0z" fill="none"/>
           <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"/>
         </svg>
         Läs mitt CV
       </a>
       <a
-        href="https://www.kvadrat.se/anlita-kvadrat/hitta-konsult/jimmy-borjesson/" alt="CV Jimmy Börjesson"
-        class="flex py-3 px-6 bg-white text-black font-bold rounded"
+        href="/docs/CV Jimmy Börjesson.docx" alt="CV Jimmy Börjesson"
+        class="flex py-3 px-6 rounded border"
       >
-        <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+        <svg class="fill-current mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
           <path fill="none" d="M0 0h24v24H0z"/>
           <path d="M18 15v3H6v-3H4v3c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-3h-2zm-1-4l-1.41-1.41L13 12.17V4h-2v8.17L8.41 9.59 7 11l5 5 5-5z"/>
         </svg>
-        CV som PDF
+        CV som DOCX
       </a>
     </div>
   </section>
