ab46ba41d117f27b024f787c2d20c9ce2629e34e
Jimmy Börjesson <jimmy@boerjesson.se>
2021-07-15 14:01:28 +0200

    Add BC logo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="flex flex-col min-h-screen bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-200">
     <div class="max-w-screen-2xl mx-auto flex-auto">
-      <header class="flex items-center justify-between px-12 pt-12">
+      <header>
         <Brand />
-        <ThemeToggle />
       </header>
 
       <main>
-        <div class="lg:px-12 lg:grid lg:grid-cols-2 lg:py-16 lg:gap-12">
+        <div class="lg:px-12 lg:grid lg:grid-cols-2 lg:gap-12">
           <Tagline />
           <Kvadrat />
         </div>
@@ -35,7 +34,6 @@ import Footer from './components/Footer.vue'
 import Kvadrat from './components/Kvadrat.vue'
 import Tagline from './components/Tagline.vue'
 import Tools from './components/Tools.vue'
-import ThemeToggle from './components/ThemeToggle.vue'
 
 export default defineComponent({
   name: 'App',
@@ -45,7 +43,6 @@ export default defineComponent({
     Kvadrat,
     Tagline,
     Tools,
-    ThemeToggle,
   }
 })
 </script>
--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -1,5 +1,26 @@
 <template>
-  <h1 class="text-4xl font-serif font-bold lg:text-5xl">
-    Börjesson Consulting AB
-  </h1>
+  <div class="p-12 flex items-start justify-between">
+    <div class="flex flex-col space-y-4 items-start sm:flex-row sm:space-y-0 sm:items-center">
+      <svg class="mr-4 w-8 h-8 lg:w-12 lg:h-12 rounded" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 100V0l37.5 25L75 50 37.5 75z" fill-rule="evenodd" fill="#222" />
+        <path d="M100 0L62.5 25 25 50l37.5 25 37.5 25V66.667L75 50l25-16.667V0z" fill="#bbb" fill-rule="evenodd"/>
+      </svg>
+      <h1 class="mr-8 text-xl font-serif font-bold md:text-3xl lg:text-4xl col-span-4">
+        Börjesson Consulting AB
+      </h1>
+    </div>
+    <ThemeToggle />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import ThemeToggle from "./ThemeToggle.vue"
+
+export default defineComponent({
+  components: {
+    ThemeToggle,
+  }
+})
+</script>
+
