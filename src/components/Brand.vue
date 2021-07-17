326c1195b413eeb6ff8a9380e5ddd22ab8b73355
Jimmy Börjesson <jimmy@boerjesson.se>
2021-07-17 16:51:31 +0200

    Fiddle with sizing and positioning of the logo and company name

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="p-12 flex items-start justify-between">
-    <div class="flex flex-col items-start sm:flex-row sm:space-y-0 sm:items-center">
-      <svg class="dark:hidden mr-4 w-8 h-8 lg:w-12 lg:h-12 rounded" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <div class="flex px-3 py-6 sm:p-12 items-center space-x-4">
+    <div class="w-8 h-8 lg:h-10 lg:w-10">
+      <svg class="dark:hidden rounded" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <path d="M0 100V0l37.5 25L75 50 37.5 75z" fill-rule="evenodd" fill="#222" />
         <path d="M100 0L62.5 25 25 50l37.5 25 37.5 25V66.667L75 50l25-16.667V0z" fill="#bbb" fill-rule="evenodd"/>
       </svg>
-      <svg class="hidden dark:block mr-4 w-8 h-8 lg:w-12 lg:h-12 rounded" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 100V0l37.5 25L75 50 37.5 75z" fill-rule="evenodd" fill="#ddd" />
-        <path d="M100 0L62.5 25 25 50l37.5 25 37.5 25V66.667L75 50l25-16.667V0z" fill="#888" fill-rule="evenodd"/>
+      <svg class="hidden dark:block rounded" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 100V0l37.5 25L75 50 37.5 75z" fill-rule="evenodd" fill="#bbb" />
+        <path d="M100 0L62.5 25 25 50l37.5 25 37.5 25V66.667L75 50l25-16.667V0z" fill="#222" fill-rule="evenodd"/>
       </svg>
-      <h1 class="mr-8 mt-4 text-xl font-serif font-bold md:text-3xl lg:text-4xl col-span-4">
-        Börjesson Consulting AB
-      </h1>
     </div>
-    <ThemeToggle />
+    <h1 class="flex-1 font-serif font-bold whitespace-nowrap text-lg sm:text-xl md:text-2xl lg:text-3xl">
+      Börjesson Consulting AB
+    </h1>
+    <div class="">
+      <ThemeToggle />
+    </div>
   </div>
 </template>
 
