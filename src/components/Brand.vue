204254b006c8f2109cc1d36a1f92b46bad1efa59
Jimmy Börjesson <jimmy@boerjesson.se>
2021-07-18 09:54:00 +0200

    Fiddle more with logo alignment

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex px-3 py-6 sm:p-12 items-center space-x-4">
-    <div class="w-8 h-8 lg:h-10 lg:w-10">
+  <div class="flex items-center py-4 sm:p-12">
+    <div class="w-12 h-12 p-2">
       <svg class="dark:hidden rounded" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <path d="M0 100V0l37.5 25L75 50 37.5 75z" fill-rule="evenodd" fill="#222" />
         <path d="M100 0L62.5 25 25 50l37.5 25 37.5 25V66.667L75 50l25-16.667V0z" fill="#bbb" fill-rule="evenodd"/>
@@ -10,10 +10,10 @@
         <path d="M100 0L62.5 25 25 50l37.5 25 37.5 25V66.667L75 50l25-16.667V0z" fill="#222" fill-rule="evenodd"/>
       </svg>
     </div>
-    <h1 class="flex-1 font-serif font-bold whitespace-nowrap text-lg sm:text-xl md:text-2xl lg:text-3xl">
+    <h1 class="flex-1 font-serif font-bold whitespace-nowrap text-center text-xl sm:text-left sm:text-2xl lg:text-3xl">
       Börjesson Consulting AB
     </h1>
-    <div class="">
+    <div class="w-12 h-12 flex justify-center items-center">
       <ThemeToggle />
     </div>
   </div>
