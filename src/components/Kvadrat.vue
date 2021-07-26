a26f03286ea386f93c6280dfd600c4a818526b9c
Jimmy Börjesson <jimmy@boerjesson.se>
2021-07-26 10:37:41 +0200

    Move kvadrat logo to assets

--- src/components/Kvadrat.vue
+++ src/components/Kvadrat.vue
@@ -1,19 +1,19 @@
 <template>
-  <section class="p-6 sm:p-12 bg-kvadrat text-gray-100 shadow-lg lg:rounded">
+  <section class="p-6 text-gray-100 shadow-lg sm:p-12 bg-kvadrat lg:rounded">
     <a href="https://www.kvadrat.se/">
-      <img class="w-40 mb-6" src="/images/kvadrat.svg" alt="Kvadrat logo" />
+      <img class="w-40 mb-6" src="../assets/kvadrat.svg" alt="Kvadrat logo" />
     </a>
 
     <p style="text-indent: 2.5rem;">
       Jag är ansluten till <strong>Kvadrat</strong>, Sveriges lyckligaste konsulter. Vi erbjuder våra kunder en unik kombination av konsultens expertis och egenföretagarens engagemang, attityd och motivation att alltid lyckas. Kvadrat bildades 1988 och idag är vi ca 500 Kvadratare med kontor i Stockholm, Göteborg, Malmö, Linköping, Norrköping, Jönköping, Växjö, Örebro, Borlänge och Sundsvall.
     </p>
 
-    <div class="flex flex-col justify-end space-y-4 sm:space-y-0 sm:flex-row sm:space-x-4 mt-6">
+    <div class="flex flex-col justify-end mt-6 space-y-4 sm:space-y-0 sm:flex-row sm:space-x-4">
       <a
         href="https://www.kvadrat.se/anlita-kvadrat/hitta-konsult/jimmy-borjesson/"
-        class="flex py-3 px-6 bg-white text-black font-bold rounded shadow hover:bg-gray-200 active:bg-gray-200"
+        class="flex px-6 py-3 font-bold text-black bg-white rounded shadow hover:bg-gray-200 active:bg-gray-200"
       >
-        <svg class="fill-current mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+        <svg class="mr-2 fill-current" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
           <path d="M0 0h24v24H0V0z" fill="none"/>
           <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"/>
         </svg>
@@ -21,9 +21,9 @@
       </a>
       <a
         href="./docs/CV Jimmy Börjesson.docx"
-        class="flex py-3 px-6 rounded border hover:bg-red-800 active:bg-red-800"
+        class="flex px-6 py-3 border rounded hover:bg-red-800 active:bg-red-800"
       >
-        <svg class="fill-current mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+        <svg class="mr-2 fill-current" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
           <path fill="none" d="M0 0h24v24H0z"/>
           <path d="M18 15v3H6v-3H4v3c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-3h-2zm-1-4l-1.41-1.41L13 12.17V4h-2v8.17L8.41 9.59 7 11l5 5 5-5z"/>
         </svg>
